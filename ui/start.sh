2683dbe084573c59939f7078d7117f12c443d6c8
Chris Crocker-White <chriscw@balena.io>
2020-04-14 15:12:27 +0100

    Fix drawing characters displaying as letters
    Change-type: patch
    Signed-off-by: Chris Crocker-White <chriscw@balena.io>

--- ui/start.sh
+++ ui/start.sh
@@ -3,6 +3,7 @@
 # start gotty with boinctui
 /usr/app/gotty -w -p 80 boinctui&
 
+export NCURSES_NO_UTF8_ACS=1
 #switch to tty2, clear screen and display boinctui 
 chvt 2
 printf "\033c" > /dev/tty2

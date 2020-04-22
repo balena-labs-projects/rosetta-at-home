0899029976655aa134a2769fa0358e070235026b
Chris Crocker-White <chriscw@balena.io>
2020-04-22 11:25:01 +0100

    Correct filenames
    Change-type: patch
    Signed-off-by: Chris Crocker-White <chriscw@balena.io>

--- boinc-client/start.sh
+++ boinc-client/start.sh
@@ -6,7 +6,7 @@ if [[ -z $ACCOUNT_KEY ]]; then
   echo 'Account key undefined - using balena key'
 else
   echo 'Account key set'
-  sed -i -e 's|<authenticator>[0-9a-z_]\{1,\}</authenticator>|<authenticator>'"$ACCOUNT_KEY"'</authenticator>|g' /usr/app/account_boinc.rosetta.bakerlab.org.xml
+  sed -i -e 's|<authenticator>[0-9a-z_]\{1,\}</authenticator>|<authenticator>'"$ACCOUNT_KEY"'</authenticator>|g' /usr/app/boinc/account_boinc.bakerlab.org_rosetta.xml
 fi
 
 totalmem=$(awk '/^MemTotal:/{print $2}' /proc/meminfo)

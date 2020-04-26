5bcaef6397e4b6ba5fa7d3e23202e8aa862c25b0
Piotrek Marciniak <piotrek@ptrm.eu>
2020-04-26 16:49:15 +0200

    Add ram settings validation and flags to disable
    Change-type: minor

--- boinc-client/start.sh
+++ boinc-client/start.sh
@@ -2,18 +2,32 @@
 mkdir -p /usr/app/boinc/locale
 mkdir -p /usr/app/boinc/slots
 
+prefs_file_path='global_prefs_override.xml'
+cfg_ram_max_busy_xml_key='ram_max_used_busy_pct'
+cfg_ram_max_idle_xml_key='ram_max_used_idle_pct'
+treshold_ram_settings_pct=95
+
+. start-utils.sh
+
+cd /usr/app/boinc
+
 if [[ -z $ACCOUNT_KEY ]]; then
   echo 'Account key undefined - using balena key'
 else
   echo 'Account key set'
-  sed -i -e 's|<authenticator>[0-9a-z_]\{1,\}</authenticator>|<authenticator>'"$ACCOUNT_KEY"'</authenticator>|g' /usr/app/boinc/account_boinc.bakerlab.org_rosetta.xml
+  sed -i -e 's|<authenticator>[0-9a-z_]\{1,\}</authenticator>|<authenticator>'"$ACCOUNT_KEY"'</authenticator>|g' account_boinc.bakerlab.org_rosetta.xml
+fi
+
+if [[ -z $SKIP_BOINC_MEM_SETTINGS_CHECK ]]; then
+  echo "Validating boinc RAM settings"
+  validate_ram_settings
 fi
 
 totalmem=$(awk '/^MemTotal:/{print $2}' /proc/meminfo)
 
-if [ "$totalmem" -lt "1500000" ]; then
+if [[ -z $SKIP_BOINC_CPU_SETTINGS_CHECK && "$totalmem" -lt "1500000" ]]; then
   echo "Less than 1.5GB RAM - running single concurrent task"
-  sed -i -e 's|<max_ncpus_pct>[0-9a-z.]\{1,\}</max_ncpus_pct>|<max_ncpus_pct>25.000000</max_ncpus_pct>|g' /usr/app/boinc/global_prefs_override.xml
+  update_float_xml_val_with_int max_ncpus_pct 25 "$prefs_file_path"
 fi
 
 exec boinc --dir /usr/app/boinc/ --allow_remote_gui_rpc

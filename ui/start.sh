7aec89cd3533f8f64b378924f11ac216a48af5fd
Tomás Migone <tomas@balena.io>
2021-05-19 10:50:46 -0300

    Update app for open fleets on hub
    Change-type: patch
    Signed-off-by: Tomás Migone <tomas@balena.io>

--- ui/start.sh
+++ ui/start.sh
@@ -1,5 +1,10 @@
 #! /bin/bash
 
+# openFleets: configure hostname
+curl -X PATCH --header "Content-Type:application/json" \
+    --data '{"network": {"hostname": "foldforcovid"}}' \
+    "$BALENA_SUPERVISOR_ADDRESS/v1/device/host-config?apikey=$BALENA_SUPERVISOR_API_KEY"
+
 # start gotty with boinctui
 /usr/app/gotty -w -p 8080 boinctui&
 
